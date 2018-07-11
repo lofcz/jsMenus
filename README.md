09975f1bc54ac36850391481bc350aa44e7c4cf8
Per Bothner <per@bothner.com>
2018-07-11 13:54:28 -0700

    Simplify to a simple single-file JavaScript package.

--- README.md
+++ README.md
@@ -38,99 +38,12 @@ this is probably a regression on Mac (needs testing).
 * Internal changes, such as when a menu is no longer shown, we remove
 its elements, rather than just hiding it with css.
 
-### Building
+### Using
 
-Install a bunch of dependencies using `npm` (FIXME)
-and then do `npm run build`.  Then you can load
-`dist/nwjs-menu-browser.js` and `dist/nwjs-menu-browser.css`
-in your browser, and then use `nwjsMenuBrowser.Menu`
-and  `nwjsMenuBrowser.MenuItem`.
+Just load or require `nwjs-menu-browser.js` and `nwjs-menu-browser.css`.
 
 ### TODO
-* Avoid build dependencies on `npm` or `webpack`.
 * Test and polish style on multiple platforms, including MacOS.
 * Internal cleanup - Menu or MenuItem should not have properties
 that depend on display/navigation state. Specifically, there
 should be no pointers to the parent or DOM node, not even temporarily.
-
-## Orginal README
-
-Browser Polyfill for [NWJS](http://docs.nwjs.io/en/latest/) [Menu](http://docs.nwjs.io/en/latest/References/Menu/) and [MenuItem](http://docs.nwjs.io/en/latest/References/MenuItem/).
-
-## Why
-
-My audio visualisation app [modV](http://github.com/2xAA/modV/) recently moved to NWJS and I had previously built my own (terrible) context menus for the browser. So as not to write two lots of code I thought I'd polyfill the browser so you could use the same code between NWJS and (presumably) Chrome!
-
-This can also be used as a regular Context Menu library if so wished 😎
-
-## Caveats
-
-Does not support ```createMacBuiltIn```, ```MenuItem.key``` or ```MenuItem.modifiers```, though usage of these will not break existing code and will be displayed in the menu nodes.
-
-These menus are not checked against any OS menu specification, but it's close enough to polyfill for the browser.
-If you'd like more accurate functionality, PRs and enhancement issues are welcome!
-
-## Usage
-
-### Install
-
-`npm i nwjs-menu-browser`
-
-### Demo
-
-Run ```npm run watch``` and a browser window pointing to ```localhost:8080``` will open.
-(the demo is not included in the NPM package, please clone from git)
-
-### Including in your project
-
-The included stylesheet in `dist` is optional, but you will need some sort of style for your menus.
-```HTML
-<link rel=stylesheet type=text/css href=nwjs-menu-browser.css>
-```
-
-#### ES6
-```JavaScript
-import { Menu, MenuItem } from 'nwjs-menu-browser';
-
-if(!nw) {
-  var nw = {
-    Menu,
-    MenuItem
-  };
-}
-```
-
-#### ES5
-```JavaScript
-if(!nw) {
-  var nw = {};
-  nw.Menu = require('nwjs-menu-browser').Menu;
-  nw.MenuItem = require('nwjs-menu-browser').MenuItem;
-}
-```
-
-#### Script Tag
-```HTML
-<script src=nwjs-menu-browser.js></script>
-<script>
-if(!nw) {
-  var nw = {};
-  nw.Menu = window.nwjsMenuBrowser.Menu;
-  nw.MenuItem = window.nwjsMenuBrowser.MenuItem;
-}
-</script>
-```
-
-### Build
-
-Prerequisites: ``npm install gulp webpack webpack-stream babel-loader babel-core es2015 babel-preset-es2015``.
-
-If required, build using ```npm run build```, built files will be in ```./dist```.
-
-## Screenshots
-The included stylesheet (```nwjs-menu-browser.css```) is a close match to macOS Sierra's menus.
-If somebody would like to contribute extra 'themes' I'd be very happy 😘
-
-![menu-bar](./example-assets/images/menu-bar.png)
-
-![menu-bar](./example-assets/images/context-menu.png)
--- nwjs-menu-browser.js
+++ nwjs-menu-browser.js
@@ -1,7 +1,3 @@
-import MenuItem from '../menu-item';
-import isDescendant from '../is-decendant';
-import recursiveNodeFind from '../recursive-node-find';
-
 class Menu {
 	constructor(settings = {}) {
 		const typeEnum = ['contextmenu', 'menubar'];
@@ -201,9 +197,20 @@ class Menu {
 		return menuNode;
 	}
 
+	static isDescendant(parent, child) {
+		let node = child.parentNode;
+		while(node !== null) {
+			if(node === parent) {
+				return true;
+			}
+			node = node.parentNode;
+		}
+		return false;
+	}
+
 	static _mouseHandler(e) {
 		let inMenubar = Menu._menubarNode != null
-                    && isDescendant(Menu._menubarNode, e.target);
+                    && Menu.isDescendant(Menu._menubarNode, e.target);
 		let menubarHandler = e.currentTarget == Menu._menubarNode;
 		let miNode = e.target;
 		while (miNode && ! miNode.jsMenuItem)
@@ -314,6 +321,36 @@ class Menu {
 		}
 	}
 
+	static recursiveNodeFind(menu, node) {
+		if(menu.node === node) {
+			return true;
+		} else if(Menu.isDescendant(menu.node, node)) {
+			return true;
+		} else if(menu.items.length > 0) {
+			for(var i=0; i < menu.items.length; i++) {
+				let menuItem = menu.items[i];
+				if(!menuItem.node) continue;
+
+				if(menuItem.node === node) {
+					return true;
+				} else if(Menu.isDescendant(menuItem.node, node)) {
+					return true;
+				} else {
+					if(menuItem.submenu) {
+						if(recursiveNodeFind(menuItem.submenu, node)) {
+							return true;
+						} else {
+							continue;
+						}
+					}
+				}
+			}
+		} else {
+			return false;
+		}
+		return false;
+	}
+
 	isNodeInChildMenuTree(node = false) {
 		if(!node) return false;
 		return recursiveNodeFind(this, node);
@@ -347,12 +384,12 @@ Menu._keydownListener = function(e) {
 		}
 	}
 	function nextMenu(forwards) {
-		let menubarNode = nwjsMenuBrowser.Menu._topmostMenu.parentMenu.node;
+		let menubarNode = Menu._topmostMenu.parentMenu.node;
 		let next = nextItem(menubarNode,
 				    menubarNode.activeItemNode,
 				    forwards);
 		if (next)
-		    next.jsMenuItem.select(next, true, true, true);
+		    next.jsMenuItem.select(next, true, true);
 		return next;
 
 	}
@@ -392,7 +429,7 @@ Menu._keydownListener = function(e) {
 					    menuNode.activeItemNode,
 					    e.keyCode == 40);
 			if (next)
-				next.jsMenuItem.select(next, true, false);
+				next.jsMenuItem.select(next, false);
 			break;
 		}
 	}
@@ -409,7 +446,334 @@ Menu._keydownListen = function(value) {
 }
 Menu._keydownListen(true);
 
-export default Menu;
+class MenuItem {
+	constructor(settings = {}) {
+
+		const modifierSymbols = {
+			shift: '⇧',
+			ctrl: '⌃',
+			alt: '⌥',
+			cmd: '⌘',
+			super: '⌘',
+			command: '⌘'
+		};
+
+		const keySymbols = {
+			up: '↑',
+			esc: '⎋',
+			tab: '⇥',
+			left: '←',
+			down: '↓',
+			right: '→',
+			pageUp: '⇞',
+			escape: '⎋',
+			pageDown: '⇟',
+			backspace: '⌫',
+			space: 'Space'
+		};
+
+		const modifiersEnum = ['cmd', 'command', 'super', 'shift', 'ctrl', 'alt'];
+		const typeEnum = ['separator', 'checkbox', 'normal'];
+		let type = isValidType(settings.type) ? settings.type : 'normal';
+		let submenu = settings.submenu || null;
+		let click = settings.click || null;
+		let modifiers = validModifiers(settings.modifiers) ? settings.modifiers : null;
+		let label = settings.label || '';
+
+		let enabled = settings.enabled;
+		if(typeof settings.enabled === 'undefined') enabled = true;
+		let visible = settings.visible;
+		if(typeof settings.visible === 'undefined') visible = true;
+
+		if(submenu) {
+			submenu.parentMenuItem = this;
+		}
+
+		Object.defineProperty(this, 'type', {
+			get: () => {
+				return type;
+			}
+		});
+
+		Object.defineProperty(this, 'submenu', {
+			get: () => {
+				return submenu;
+			},
+			set: (inputMenu) => {
+				console.warn('submenu should be set on initialisation, changing this at runtime could be slow on some platforms.');
+				if(!(inputMenu instanceof Menu)) {
+					console.error('submenu must be an instance of Menu');
+					return;
+				} else {
+					submenu = inputMenu;
+					submenu.parentMenuItem = this;
+				}
+			}
+		});
+
+		Object.defineProperty(this, 'click', {
+			get: () => {
+				return click;
+			},
+			set: (inputCallback) => {
+				if(typeof inputCallback !== 'function') {
+					console.error('click must be a function');
+					return;
+				} else {
+					click = inputCallback;
+				}
+			}
+		});
+
+		Object.defineProperty(this, 'modifiers', {
+			get: () => {
+				return modifiers;
+			},
+			set: (inputModifiers) => {
+				modifiers = validModifiers(inputModifiers) ? inputModifiers : modifiers;
+			}
+		});
+
+		Object.defineProperty(this, 'enabled', {
+			get: () => {
+				return enabled;
+			},
+			set: (inputEnabled) => {
+				enabled = inputEnabled;
+			}
+		});
+
+		Object.defineProperty(this, 'visible', {
+			get: () => {
+				return visible;
+			},
+			set: (inputVisible) => {
+				visible = inputVisible;
+			}
+		});
+
+		Object.defineProperty(this, 'label', {
+			get: () => {
+				return label;
+			},
+			set: (inputLabel) => {
+				label = inputLabel;
+			}
+		});
+
+		this.icon = settings.icon || null;
+		this.iconIsTemplate = settings.iconIsTemplate || false;
+		this.tooltip = settings.tooltip || '';
+		this.checked = settings.checked || false;
+
+		this.key = settings.key || null;
+		this.accelerator = settings.accelerator;
+		this.node = null;
+
+		if(this.key) {
+			this.key = this.key.toUpperCase();
+		}
+		function validModifiers(modifiersIn = '') {
+			let modsArr = modifiersIn.split('+');
+			for(let i=0; i < modsArr; i++) {
+				let mod = modsArr[i].trim();
+				if(modifiersEnum.indexOf(mod) < 0) {
+					console.error(`${mod} is not a valid modifier`);
+					return false;
+				}
+			}
+			return true;
+		}
+
+		function isValidType(typeIn = '', debug = false) {
+			if(typeEnum.indexOf(typeIn) < 0) {
+				if(debug) console.error(`${typeIn} is not a valid type`);
+				return false;
+			}
+			return true;
+		}
+	}
+
+	toString() {
+		return this.type+"["+this.label+"]";
+	}
+
+	_mouseoverHandle_menubarTop() {
+		let pmenu = this.parentMenu.node;
+		if (pmenu.activeItemNode) {
+			pmenu.activeItemNode.classList.remove('active');
+			pmenu.activeItemNode = null;
+		}
+		if (pmenu && pmenu.querySelector('.submenu-active')) {
+			if(this.node.classList.contains('submenu-active')) return;
+
+			this.parentMenu.clearActiveSubmenuStyling(this.node);
+			this.node.classList.add('submenu-active');
+			this.select(this.node, true, true);
+		}
+	}
+
+	doit() {
+		if (! this.submenu) {
+			this.parentMenu.popdownAll();
+			if(this.type === 'checkbox')
+				this.checked = !this.checked;
+			if(this.click) this.click(this);
+		}
+	}
+
+	select(node, popupSubmenu, menubarSubmenu = false) {
+		let pmenu = this.parentMenu.node;
+		if (pmenu.activeItemNode) {
+			pmenu.activeItemNode.classList.remove('active');
+			pmenu.activeItemNode = null;
+		}
+		if(pmenu.currentSubmenu) {
+			pmenu.currentSubmenu.popdown();
+			pmenu.currentSubmenu.parentMenuItem.node.classList.remove('submenu-active');
+			pmenu.currentSubmenu = null;
+                }
+		if(this.submenu && popupSubmenu)
+			this.selectSubmenu(node, menubarSubmenu);
+		else
+			node.classList.add('active');
+		this.parentMenu.node.activeItemNode = this.node;
+	}
+
+	selectSubmenu(node, menubarSubmenu) {
+		this.parentMenu.node.currentSubmenu = this.submenu;
+		if(this.submenu.node)
+			return;
+
+		let parentNode = node.parentNode;
+		let x, y;
+		if (menubarSubmenu) {
+			x = node.offsetLeft;
+			y = node.clientHeight;
+		} else {
+			x = parentNode.offsetWidth + parentNode.offsetLeft - 2;
+			y = parentNode.offsetTop + node.offsetTop - 4;
+		}
+		this.popupSubmenu(x, y, menubarSubmenu);
+		node.classList.add('submenu-active');
+	}
+
+	buildItem(menuNode, menuBarTopLevel = false) {
+		let node = document.createElement('li');
+		node.jsMenuNode = menuNode;
+		node.jsMenu = menuNode.jsMenu;
+		node.jsMenuItem = this;
+		node.classList.add('menu-item', this.type);
+
+		menuBarTopLevel = menuBarTopLevel || this.menuBarTopLevel || false;
+		this.menuBarTopLevel = menuBarTopLevel;
+
+		if(menuBarTopLevel) {
+			node.addEventListener('mouseenter', this._mouseoverHandle_menubarTop.bind(this));
+		}
+
+		let iconWrapNode = document.createElement('div');
+		iconWrapNode.classList.add('icon-wrap');
+
+		if(this.icon) {
+			let iconNode = new Image();
+			iconNode.src = this.icon;
+			iconNode.classList.add('icon');
+			iconWrapNode.appendChild(iconNode);
+		}
+
+		let labelNode = document.createElement('div');
+		labelNode.classList.add('label');
+
+		let modifierNode = document.createElement('div');
+		modifierNode.classList.add('modifiers');
+
+		let checkmarkNode = document.createElement('div');
+		checkmarkNode.classList.add('checkmark');
+
+		if(!menuBarTopLevel) {
+                    if (this.checked)
+			node.classList.add('checked');
+                    else
+			node.classList.remove('checked');
+		}
+
+		let text = '';
+
+		if(this.submenu && !menuBarTopLevel) {
+			text = '▶︎';
+
+			node.addEventListener('mouseleave', (e) => {
+				if(node !== e.target) {
+					if(!Menu.isDescendant(node, e.target))
+						this.submenu.popdown();
+				}
+			});
+		}
+
+		if(this.modifiers && !menuBarTopLevel) {
+			let mods = this.modifiers.split('+');
+
+			// Looping this way to keep order of symbols - required by macOS
+			for(let symbol in modifierSymbols) {
+				if(mods.indexOf(symbol) > -1) {
+					text += modifierSymbols[symbol];
+				}
+			}
+		}
+
+		if(this.key && !menuBarTopLevel) {
+			text += this.key;
+		}
+		if (this.accelerator && !menuBarTopLevel) {
+			let acc = this.accelerator;
+                    let mac = false; // FIXME
+                    let cmd = mac ? "Cmd" : "Ctrl";
+                    acc = acc.replace("CommandOrControl", cmd);
+                    acc = acc.replace("Mod+", cmd+"+");
+			text += acc;
+		}
+
+		if(!this.enabled) {
+			node.classList.add('disabled');
+		}
+
+		if(!menuBarTopLevel) {
+			node.addEventListener('mouseenter', () => {
+				this.select(node, true);
+			});
+		}
+
+		if(this.icon) labelNode.appendChild(iconWrapNode);
+
+		let textLabelNode = document.createElement('span');
+		textLabelNode.textContent = this.label;
+		textLabelNode.classList.add('label-text');
+
+		node.appendChild(checkmarkNode);
+
+		labelNode.appendChild(textLabelNode);
+		node.appendChild(labelNode);
+
+		modifierNode.appendChild(document.createTextNode(text));
+		node.appendChild(modifierNode);
+
+		node.title = this.tooltip;
+		this.node = node;
+		return node;
+	}
+
+	popupSubmenu(x, y, menubarSubmenu = false) {
+		this.submenu.popup(x, y, true, menubarSubmenu);
+		this.submenu.node.menuItem = this.node;
+		this.parentMenu.node.currentSubmenu = this.submenu;
+	}
+}
+
+if (typeof module !== "undefined" && module.exports) {
+	module.exports = { Menu: Menu, MenuItem: MenuItem };
+}
+
 // Local Variables:
 // js-indent-level: 8
 // indent-tabs-mode: t

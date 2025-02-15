5737ef938b3b24aef61c76a4d712fe5b1c289029
Matěj Štágl <stagl.matej@centrum.cz>
2025-02-15 18:28:46 +0100

    setup umd export

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,34 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const wrap = require('gulp-wrap');
 
-// JavaScript task
 gulp.task('scripts', function() {
     return gulp.src('jsMenus.js')
+        .pipe(wrap(`
+            (function(root, factory) {
+                if (typeof define === 'function' && define.amd) {
+                    define([], factory);
+                } else if (typeof exports === 'object') {
+                    module.exports = factory();
+                } else {
+                    root.JsMenuLib = factory();
+                }
+            }(typeof self !== 'undefined' ? self : this, function() {
+                <%= contents %>
+                return {
+                    Menu: Menu,
+                    MenuItem: MenuItem
+                };
+            }));
+        `))
+        .pipe(rename({ suffix: '.umd' }))
+        .pipe(gulp.dest('dist'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist'));
 });
 
-// CSS task
 gulp.task('styles', function() {
     return gulp.src('jsMenus.css')
         .pipe(cleanCSS())
@@ -19,8 +37,5 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('dist'));
 });
 
-// Build task
 gulp.task('build', gulp.parallel('scripts', 'styles'));
-
-// Default task
 gulp.task('default', gulp.series('build'));
